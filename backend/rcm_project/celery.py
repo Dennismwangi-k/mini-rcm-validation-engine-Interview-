1f6c841cfadc62ff9e08d7a6fb297c1cb306b3d7
Dennismwangi-k <denixxdenixx64@gmail.com>
2025-11-16 03:26:12 +0300

    sd

--- backend/rcm_project/celery.py
+++ backend/rcm_project/celery.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from celery import Celery
 from django.conf import settings
 
@@ -7,6 +8,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rcm_project.settings')
 app = Celery('rcm_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# Fix for macOS fork() issue - use solo pool on macOS
+if sys.platform == 'darwin':
+    app.conf.worker_pool = 'solo'
 
 @app.task(bind=True)
 def debug_task(self):
